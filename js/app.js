de4c8673036b5b6f0b0a41a37248b77b08571728
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-25 13:00:01 -0300

    show results

--- js/app.js
+++ js/app.js
@@ -71,15 +71,70 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
     div.textContent = mensaje;
 
     const formulario = document.querySelector('#cotizar-seguro');
+    ui.limpiar();
     formulario.insertBefore(div, document.querySelector('#resultado'));
-
+    
     setTimeout(() => {
         div.remove();
-    }, 2000)
+    }, 3000)
+}
+
+UI.prototype.mostrarResultado = (seguro, total) => {
+    const div = document.createElement('div');
+    const {marca, year, tipo} = seguro;
+    let marcaString = '';
+    switch(marca) {
+        case '1':
+            marcaString = 'Americano';
+            break;
+        case '2':
+            marcaString = 'Asiatico';
+            break;
+        case '3':
+            marcaString = 'Europeo';
+            break;
+        default:
+            break;
+    }
+    div.classList.add('mt-10');
+    div.id = 'resumen';
+    div.innerHTML = `
+        <p class="header">Tu resumen</p>
+        <p class="font-bold">Marca: <span class="font-normal">${marcaString}</span></p>
+        <p class="font-bold">Año: <span class="font-normal">${year}</span></p>
+        <p class="font-bold">Tipo: <span class="font-normal capitalize">${tipo}</span></p>
+        <p class="font-bold">Total: <span class="font-normal">$ ${total}</span></p>
+    `
+    const resultadoDiv = document.querySelector('#resultado');
+    const spinner = document.querySelector('#cargando');
+    spinner.style.display = 'block';
+    setTimeout(() => {
+        spinner.style.display = 'none';
+        ui.limpiar();
+        resultadoDiv.appendChild(div);
+        ui.formswitch();
+    }, 3000)
+}
+
+UI.prototype.formswitch = () => {
+    var inputs = document.getElementsByTagName("input"); 
+    for (var i = 0; i < inputs.length; i++) { 
+        inputs[i].disabled = !inputs[i].disabled;
+    } 
+    var selects = document.getElementsByTagName("select");
+    for (var i = 0; i < selects.length; i++) {
+        selects[i].disabled = !selects[i].disabled;
+    }
+    var buttons = document.getElementsByTagName("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = !buttons[i].disabled;
+    }
 }
 
 UI.prototype.limpiar = () => {
     const mensajes = document.querySelectorAll('.mensaje');
+    const resumen = document.querySelectorAll('#resumen')
+    resumen.forEach(r => r.remove());
     mensajes.forEach(msg => msg.remove());
 }
 
@@ -101,12 +156,10 @@ function eventListeners() {
 function cotizarSeguro(e) {
     e.preventDefault();
     // Leer marca año y tipo de cobertura
-
+    
     const marca = document.querySelector('#marca').value;
     const year = document.querySelector('#year').value;
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
-
-    ui.limpiar();
 
     if (marca === '' || year === '' || tipo === '') {
         ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
@@ -116,5 +169,8 @@ function cotizarSeguro(e) {
     ui.mostrarMensaje('Cotizando ...', 'exito');
 
     const seguro = new Seguro(marca, year, tipo);
-    seguro.cotizar();
+    const total = seguro.cotizar();
+    ui.formswitch();
+    ui.mostrarResultado(seguro, total);
 }
+
