993e9faf4fd791679259fb10c38ab25e2a660a41
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-25 11:25:52 -0300

    Form validation

--- js/app.js
+++ js/app.js
@@ -29,3 +29,25 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones();
 });
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+    // Leer marca año y tipo de cobertura
+
+    const marca = document.querySelector('#marca').value;
+    const year = document.querySelector('#year').value;
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if(marca === '' || year === '' || tipo === '') {
+        // Mostrar mensaje de error
+    }else {
+        // Realizar cotizacion
+    }
+}
